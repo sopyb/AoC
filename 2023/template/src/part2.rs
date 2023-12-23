35348ecb9ea243d5666af35fd60294540085be6b
sopy <contact@sopy.one>
2023-12-23 11:27:49 +0200

    Template added for 2023

---
+++ 2023/template/src/part2.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // read input.in
+    let input = std::fs::read_to_string("src/input.in").unwrap();
+
+    // split input into lines
+    let lines = input.lines();
+
+    // for each line
+    for line in lines {
+
+    }
+}
