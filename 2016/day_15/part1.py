1b9f2e0628be032b6ab2d199f3e72089b8c80509
sopy <contact@sopy.one>
2024-02-13 15:04:21 +0200

    Solution 2016 Day 15

---
+++ 2016/day_15/part1.py
@@ -0,0 +1,24 @@
+import re
+
+# open and read the file to a string
+with open("input.in", "r") as file:
+    lines = file.read().split("\n")
+
+discs = []
+for line in lines:
+    match = re.match(r"Disc #\d+ has (\d+) positions; at time=0, it is at position (\d+).", line)
+    positions = int(match.group(1))
+    initial_position = int(match.group(2))
+    discs.append((positions, initial_position))
+
+time = 0
+while True:
+    for i, (positions, initial_position) in enumerate(discs, start=1):
+        position = (initial_position + time + i) % positions
+        if position != 0:
+            break
+    else:
+        break
+    time += 1
+
+print(time)
--- 2016/template/part1.py
+++ 2016/template/part1.py
@@ -1,4 +1,3 @@
 # open and read the file to a string
-input = open("input.in", "r")
-input = input.read()
-input = input.split("\n")
+with open("input.in", "r") as file:
+    lines = file.read().split("\n")
