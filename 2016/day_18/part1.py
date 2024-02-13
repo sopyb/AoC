7744793ed4d3d479218721f1a2e7b47665fc6185
sopy <contact@sopy.one>
2024-02-13 15:46:55 +0200

    Solution 2016 Day 18

---
+++ 2016/day_18/part1.py
@@ -0,0 +1,20 @@
+# open and read the file to a string
+with open("input.in", "r") as file:
+    lines = file.read()
+row_count = 40
+
+rows = [lines]
+for _ in range(row_count - 1):
+    previous_row = '.' + rows[-1] + '.'
+    next_row = ''
+    for i in range(1, len(previous_row) - 1):
+        pattern = previous_row[i-1:i+2]
+        if pattern in ['^^.', '.^^', '^..', '..^']:
+            next_row += '^'
+        else:
+            next_row += '.'
+    rows.append(next_row)
+
+safe_tiles = sum(row.count('.') for row in rows)
+
+print(safe_tiles)
